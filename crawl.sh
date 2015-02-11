f5b8c9fcfde642453c7351746f1570bc79affd42
Pablo Seibelt <pabloseibelt@gmail.com>
2015-02-11 16:12:05 -0300

    Fixed crawl script

--- crawl.sh
+++ crawl.sh
@@ -2,10 +2,12 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd $DIR/bcra
+
+cd $DIR
 . $DIR/bin/activate
 mkdir out
 rm out/*.json
+cd bcra
 
 scrapy crawl bcra_spider -o ../out/bcra.json -t json
 cd ..
